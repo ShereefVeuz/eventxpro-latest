173418aa34b24ba7109cf22aaa85eaf61d1c8621
shereef <shareef@veuz.in>
2025-09-10 11:59:36 +0530

    eventxpro latest4 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -445,6 +445,20 @@ if (document.getElementById("portfolio")) {
 }
 
 
+// return scroll
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
 
 
 
